45a599ea2f91cbaf7d99421a8e2dd2e7bb27a2de
B4rsam <105477761+B4rsam@users.noreply.github.com>
2023-11-13 15:58:12 +0330

    Added buttons

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+  <div class="main">
+    <div class="resultbox" style="background-color: white>
+
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <div class="buttonbox"> 
+      <button id="1">1</button>
+      <button id="2">2</button>
+      <button id="3">3</button>
+      <button id="4">4</button>
+      <button id="5">5</button>
+      <button id="6">6</button>
+      <button id="7">7</button>
+      <button id="8">8</button>
+      <button id="9">9</button>
+      <button id="0">0</button>
+      <button id="+">+</button>
+      <button id="-">-</button>
+      <button id="x">x</button>
+      <button id="/">/</button>
+      <button id="=">=</button>
+      <button id="C">C</button>
+    </div>
   </div>
 `
+setupCounter(document.getElementById('button'))
 
-setupCounter(document.querySelector('#counter'))
+console.log(document.getElementsByClassName("test"))
